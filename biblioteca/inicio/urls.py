0d0b8b0641bf810c4b6bd8c84a801468ff04159e
Sandro <sandrocoelho2023a@gmail.com>
2025-05-15 18:56:52 -0300

    adicionando login/logout

--- biblioteca/inicio/urls.py
+++ biblioteca/inicio/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.teste, name="teste")
+    path("", views.inicio, name="inicio"),
+    path("login", views.login_a, name="login"),
+    path("logout", views.logout_a, name="logout"),
+    path("index", views.index, name="index")
 ]
--- biblioteca/inicio/views.py
+++ biblioteca/inicio/views.py
@@ -1,7 +1,37 @@
 from django.shortcuts import render
 from .models import Autor, Livro, Usuario
+from django.contrib.auth import authenticate, login, logout
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 
 
-def teste(request):
-    return render(request, "inicio/layout.html")
+def login_a(request):
+    if request.method == "POST":
+        username = request.POST['tlogin']
+        password = request.POST['tsenha']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user:
+            login(request, user)
+            return HttpResponseRedirect(reverse('index'))
+        return render(request, 'inicio/erro.html', {"mensagem": "Não foi possível fazer o login"})
+    return render(request, "inicio/login.html")
+
+
+def logout_a(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('login'))
+
+
+def index(request):
+    return render(request, 'inicio/index.html')
+
+
+def erro(request):
+    return render(request, 'inicio/erro.html')
+
+
+def inicio(request):
+    return render(request, "inicio/inicio.html")
